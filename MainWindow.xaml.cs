b14234609cca9be61fbf27f298064a470143d9ab
Acer <Acer@DESKTOP-PEBUJ0T>
2023-10-25 23:34:41 +0300

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lab1Sophie_WPF
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Rhombus _rhombus;
+        private int _countCalculate = 0;
+        public MainWindow()
+        {
+            InitializeComponent();
+            _rhombus = new Rhombus();
+        }
+
+        private void btnCalculate_Click(object sender, RoutedEventArgs e)
+        {
+            if(double.TryParse(tbSideLength.Text, out double sideLength))
+            {
+                _rhombus.SideLength = sideLength;
+            }
+            else if(tbSideLength.Text.Length == 0)
+            {
+                _rhombus = new Rhombus();
+            }
+            else
+            {
+                MessageBox.Show("Number is not valid!", "Error!", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            MessageBox.Show($"Result after calculating: {_rhombus.CalculateArea()}", "Result!", MessageBoxButton.OK, MessageBoxImage.Information);
+            _countCalculate++;
+
+        }
+
+        private void btnCalculatePrev_Click(object sender, RoutedEventArgs e)
+        {
+            if (_countCalculate > 0)
+            {
+                MessageBox.Show($"Result after calculating: {_rhombus.CalculateArea()}", "Result!", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            else
+            {
+                MessageBox.Show("No previous calculation!", "Error!", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
---
+++ Rhombus.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1Sophie_WPF
+{
+    public class Rhombus
+    {
+        private double sideLength;
+        public double SideLength
+        {
+            get
+            {
+                return sideLength;
+            }
+            set
+            {
+                if (sideLength <= 0)
+                {
+                    throw new ArgumentException("Error! Length must be > than 0!");
+                }
+                sideLength = value;
+            }
+        }
+
+        public Rhombus()
+        {
+            sideLength = 1;
+        }
+
+        public Rhombus(double sideLength)
+        {
+            if(sideLength <= 0)
+            {
+                throw new ArgumentException("Error! Length must be > than 0!");
+            }
+            this.sideLength = sideLength;
+        }
+
+        public Rhombus(Rhombus rhombus)
+        {
+            this.sideLength = rhombus.sideLength;
+        }
+
+        public double CalculateArea()
+        {
+            return Math.Pow(sideLength, 2) * Math.Sin(Math.PI / 3);
+        }
+
+    }
+}
